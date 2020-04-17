167dcd0420a2cf9afc8e285c8a5839ed223c17c5
graciani23 <gracianigss@gmail.com>
2020-04-17 19:58:08 -0300

    merge

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,27 +22,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create($this->tableName, function (Blueprint $table) {
             $table->increments('id');
-<<<<<<< HEAD
-
-            $table->index(["detalhe_empresa_id"], 'fk_table1_DETALHES_EMPRESA1_idx');
-
-            $table->unique(["senha"], 'senha_UNIQUE');
-
-            $table->unique(["email_login"], 'email_login_UNIQUE');
-
-            $table->integer('detalhe_empresa_id', 'fk_table1_DETALHES_EMPRESA1_idx')->unsigned();
-            $table->foreign('detalhe_empresa_id', 'fk_table1_DETALHES_EMPRESA1_idx')
-                ->references('id')->on('detalhes_empresas')
-                ->onDelete('no action')
-                ->onUpdate('no action');
-=======
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
->>>>>>> a57d913fc6716559111390801570bb26d7b010aa
         });
     }
 

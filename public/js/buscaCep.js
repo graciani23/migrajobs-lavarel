4565166951d58621f89fe8c9e156465a180a133a
graciani23 <gracianigss@gmail.com>
2020-04-19 16:54:00 -0300

    api cep

---
+++ public/js/buscaCep.js
@@ -0,0 +1,19 @@
+let cep = document.querySelector('#cep')
+let rua = document.querySelector('#rua')
+let complemento = document.querySelector('#complemento')
+let cidade = document.querySelector('#cidade')
+let uf = document.querySelector('#uf')
+
+cep.onkeyup = async (event)=> {
+
+    if(cep.value.length == 8) {
+        console.log("Faz a pesquisa na api")
+        let resposta = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+        let dadosDoCep = await resposta.json()
+
+        rua.value = dadosDoCep.logradouro
+        complemento.value = dadosDoCep.complemento
+        cidade.value = dadosDoCep.localidade
+        uf.value = dadosDoCep.uf
+    }
+}
